55a3540184a39b52b09bd5534ccb0e147e7b2d65
Stas <stasccb@gmail.com>
2021-07-30 00:48:50 +0300

    Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import {tripMenu} from './view/trip-menu.js';
 import {tripFilters} from './view/trip-filters.js';
-import {tripSort} from './view/trip-sort.js';
-import {tripContent} from './view/trip-content.js';
 import {tripInfo} from './view/trip-info.js';
-import {tripCities} from './view/trip-cities.js';
+import {tripRoute} from './view/trip-route.js';
 import {tripCost} from './view/trip-cost.js';
+import {tripSort} from './view/trip-sort.js';
+import {editPoint} from './view/edit-point.js';
+import {tripContent} from './view/trip-content.js';
 
 const bodyDocument = document.querySelector('body');
 
@@ -15,7 +16,8 @@ const renderComponent = (container, component, place) => {
 renderComponent(bodyDocument.querySelector('.trip-controls__navigation'), tripMenu(), 'beforeend');
 renderComponent(bodyDocument.querySelector('.trip-controls__filters'), tripFilters(), 'beforeend');
 renderComponent(bodyDocument.querySelector('.trip-main'), tripInfo(), 'beforeend');
-renderComponent(bodyDocument.querySelector('.trip-info'), tripCities(), 'beforeend');
+renderComponent(bodyDocument.querySelector('.trip-info'), tripRoute(), 'beforeend');
 renderComponent(bodyDocument.querySelector('.trip-info'), tripCost(), 'beforeend');
 renderComponent(bodyDocument.querySelector('.trip-events'), tripSort(), 'beforeend');
+renderComponent(bodyDocument.querySelector('.trip-events'), editPoint(), 'beforeend');
 renderComponent(bodyDocument.querySelector('.trip-events'), tripContent(), 'beforeend');
--- src/view/trip-route.js
+++ src/view/trip-route.js
@@ -1,6 +1,6 @@
-const tripCities = () => (`<div class="trip-info__main">
+const tripRoute = () => (`<div class="trip-info__main">
 <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
 <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
 </div>`);
 
-export {tripCities};
+export {tripRoute};
