cb9099b1220c15df4cb6e9c941c90fa13b4a88ca
Stanislav <stasccb@gmail.com>
2021-08-11 18:53:43 +0300

    Революция или эволюция? (часть 1)

--- src/view/trip-point-add-edit.js
+++ src/view/trip-point-add-edit.js
@@ -103,10 +103,10 @@ const createTripPointAddEditTemplate = (point) => (`<li class="trip-events__item
 
     <div class="event__field-group  event__field-group--time">
       <label class="visually-hidden" for="event-start-time-1">From</label>
-      <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dayjs(point.startDateTime).format('DD[/]MM[/]YYYY HH[:]mm')}">
+      <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dayjs(point.startDateTime).format('DD[/]MM[/]YY HH[:]mm')}">
       &mdash;
       <label class="visually-hidden" for="event-end-time-1">To</label>
-      <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dayjs(point.picturesendDateTime).format('DD[/]MM[/]YYYY HH[:]mm')}">
+      <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dayjs(point.picturesendDateTime).format('DD[/]MM[/]YY HH[:]mm')}">
     </div>
 
     <div class="event__field-group  event__field-group--price">
