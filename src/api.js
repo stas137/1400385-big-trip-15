c64f06a72553a1bd17899a5ed88bee6eea46dd26
Stas <stasccb@gmail.com>
2021-09-14 14:54:16 +0300

    Пришёл, увидел, загрузил (часть 1)

---
+++ src/api.js
@@ -0,0 +1,67 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+const SuccessStatusHTTPRange = {
+  MIN: 200,
+  MAX: 299,
+};
+
+export default class Api {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getPoints() {
+    return this._load({url: 'points'})
+      .then(Api.toJSON);
+  }
+
+  updatePoints(point) {
+    return this._load({
+      url: `points/${point.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(point),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    })
+      .then(Api.toJSON);
+  }
+
+  _load({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) {
+    headers.append('Authorization', this._authorization);
+
+    return fetch(
+      `${this._endPoint}/${url}`, 
+      {
+        method, 
+        body, 
+        headers,
+      },
+    )
+      .then(Api.checkStatus)
+      .catch(Api.catchError);
+  }
+
+  static checkStatus(responce) {
+    if (responce.status < SuccessStatusHTTPRange.MIN || responce.status > SuccessStatusHTTPRange.MAX) {
+      throw new Error(`${responce.status}: ${responce.statusText}`);
+    }
+
+    return responce;
+  }
+
+  static toJSON(responce) {
+    return responce.json();
+  }
+
+  static catchError(err) {
+    throw err;
+  }
+}
